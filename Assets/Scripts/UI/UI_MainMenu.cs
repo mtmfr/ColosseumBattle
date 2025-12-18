24bebb9df9f87a2133ff17be6aaeafdb7668765b
mtmfr <mmathis1806@gmail.com>
2025-12-18 14:45:51 +0100

    Added credits
    Added credits
    Moved project to 6000.0.60f1

--- Assets/Scripts/UI/UI_MainMenu.cs
+++ Assets/Scripts/UI/UI_MainMenu.cs
@@ -1,23 +1,36 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEditor;
+using UnityEngine.EventSystems;
+using TMPro;
+using Unity.Collections;
 
-public class UI_MainMenu : MonoBehaviour
+public class UI_MainMenu : MonoBehaviour, IPointerClickHandler
 {
+    [Header("MenuWindows")]
     [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject credits;
 
+    [Header("Buttons")]
     [SerializeField] private Button startButton;
-    [SerializeField] private Button quitButton;
+    [Space]
+    [SerializeField] private Button creditsButton;
+    [SerializeField] private Button quitCreditsButton;
 
     private void OnEnable()
     {
         startButton.onClick.AddListener(StartGame);
-        quitButton.onClick.AddListener(QuitGame);
+
+        creditsButton.onClick.AddListener(ShowCredits);
+        quitCreditsButton.onClick.AddListener(HideCredits);
     }
 
     private void OnDisable()
     {
         startButton.onClick.RemoveAllListeners();
+
+        creditsButton.onClick.RemoveAllListeners();
+        quitCreditsButton.onClick.RemoveAllListeners();
     }
 
     private void StartGame()
@@ -25,15 +38,38 @@ public class UI_MainMenu : MonoBehaviour
         GameManager.UpdateGameState(GameState.Start);
     }
 
-    private void QuitGame()
+    private void ShowCredits()
     {
-#if UNITY_WEBGL
+        credits.SetActive(true);
+        mainMenu.SetActive(false);
+    }
 
-#elif UNITY_EDITOR
-        EditorApplication.isPlaying = false;
-#else
-        Application.Quit();
-#endif
+    private void HideCredits()
+    {
+        credits.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        GameObject interactedObject = eventData.pointerPressRaycast.gameObject;
+
+        if (interactedObject == null)
+            return;
+
+        if (!interactedObject.TryGetComponent(out TMP_Text text))
+            return;
+
+        int linkId = TMP_TextUtilities.FindIntersectingLink(text, eventData.position, null);
+        if (linkId == -1)
+            return;
+
+        TMP_LinkInfo linkInfo = text.textInfo.linkInfo[linkId];
+
+        string linkToOpen = linkInfo.GetLinkID();
+
+        if (linkToOpen.Contains("https"))
+            Application.OpenURL(linkToOpen);
     }
 
     public void SetActive(GameState gameState)
